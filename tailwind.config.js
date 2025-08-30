244f46251cb7ecfe84be4ad6869d214739cb90d7
chrisrenshaw79 <chrisrenshaw79@gmail.com>
2025-08-30 16:52:40 +0000

    Downgrade to stable Tailwind CSS v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        'inter': ['Inter', 'sans-serif'],
-        'poppins': ['Poppins', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
