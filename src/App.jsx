d6c759cb1a5029047f06294b048e97a7d1c56c0f
chrisrenshaw79 <chrisrenshaw79@gmail.com>
2025-09-06 15:43:40 +0000

    Using HashRouter rather than BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 import CaregiverDashboard from './components/CaregiverDashboard';
